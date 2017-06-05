3bd06335319000f510001671d0370387e1e4f602
Corey <developer.coreycaplan3@gmail.com>
2017-06-04 21:30:21 -0400

    Added notifications for messages

--- functions/index.js
+++ functions/index.js
@@ -31,16 +31,56 @@ exports.createChat = functions.database.ref("userChats/{userId}/{chatId}")
             .then(() => {
                 return admin.database().ref(`userChats/${creatorId}/${chatId}`).set(chatObject);
             })
+            .then(() => {
+                const chatMembers = {};
+                chatMembers[creatorId] = true;
+                chatMembers[otherId] = true;
+                return admin.database().ref(`chats/${chatId}/chatMembers`).set(chatMembers);
+            });
     });
 
 exports.createMessage = functions.database.ref("userMessages/{uid}/{chatId}/{messageId}")
     .onWrite(event => {
-        if(!event.data.exists()) {
+        if (!event.data.exists()) {
             return Promise.resolve(() => null);
         }
 
         const chatId = event.params.chatId;
         const messageId = event.params.messageId;
         const object = event.data.val();
-        return admin.database().ref(`chats/${chatId}/${messageId}`).set(object);
+        const chatObject = {};
+        return admin.database().ref(`chats/${chatId}/${messageId}`).set(object)
+            .then(() => {
+                return admin.database().ref("userChats").child(event.params.uid).child(chatId)
+                    .once("value", snapshot => {
+                        chatObject.creatorId = snapshot.child("creatorId").val();
+                        chatObject.otherId = snapshot.child("otherId").val();
+                        chatObject[chatObject.creatorId] = snapshot.child(chatObject.creatorId).val();
+                        chatObject[chatObject.otherId] = snapshot.child(chatObject.otherId).val();
+                    });
+            })
+            .then(() => {
+                const userIdOfNotification = chatObject.otherId === event.params.uid ? chatObject.creatorId : chatObject.otherId;
+                return admin.database().ref("userTokens").child(userIdOfNotification).once("value");
+            })
+            .then(snapshot => {
+                const tokens = [];
+                snapshot.forEach(childSnapshot => {
+                    tokens.push(childSnapshot.key);
+                });
+
+                if (tokens.length > 0) {
+                    const userIdOfNotification = chatObject.otherId === event.params.uid ?
+                        chatObject.creatorId : chatObject.otherId;
+
+                    const dataObject = {
+                        text: chatObject[userIdOfNotification].name + ": " + object.text
+                    };
+
+                    return admin.messaging().sendToDevice(tokens, {data: dataObject});
+                }
+            })
+            .then(response => {
+                console.log("Response: ", response);
+            });
     });
